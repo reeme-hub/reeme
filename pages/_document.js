c9798c4a8df0c3f013bd6e41d8c201d768be7027
reeme-hub <misterwick0@gmail.com>
2024-06-24 20:10:45 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,30 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-     {/* Add DelightChat WhatsApp Widget script before the closing body tag */}
-        <Script
-          src="https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script id="whatsapp-widget" strategy="beforeInteractive">
-          {`
-            var wa_btnSetting = {
-              btnColor: "#16BE45",
-              ctaText: "WhatsApp Us",
-              cornerRadius: 40,
-              marginBottom: 20,
-              marginLeft: 20,
-              marginRight: 20,
-              btnPosition: "left",
-              whatsAppNumber: "+919419352015",
-              welcomeMessage: "Welcome to ReeMeAdventure",
-              zIndex: 999999,
-              btnColorScheme: "light"
-            };
-            window.onload = () => {
-              _waEmbed(wa_btnSetting);
-            };
-          `}
+     
         </Script>
       </body>
     </Html>
